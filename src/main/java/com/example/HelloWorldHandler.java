32d9fe94f41e81ccf1f45f4684b3bf3918690079
mandheermaan <82092405+mandheermaan@users.noreply.github.com>
2022-08-09 15:34:26 +0530

    Create HelloWorldHandler.java

---
+++ src/main/java/com/example/HelloWorldHandler.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class HelloWorldHandler implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        return "Hello from Lambda!" + input;
+    }
+    
+}
